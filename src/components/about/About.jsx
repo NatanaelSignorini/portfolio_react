abdbe96f9f6ff5ec89070b18f6816f84ff816a28
Natanael Signorini <natanaelsignorini01@gmail.com>
2022-10-04 21:34:18 -0300

    about + experience 100%

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './about.css'
+import image_me from '../../assets/me4.png'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'> About </section>
+    <section id='about'>
+       <h5>Get To Know</h5>
+       <h2>About Me</h2>
+
+      <div className="container about__container">
+       
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={image_me} alt="About_Image"></img> 
+          </div> 
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small> 3+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>Clients</h5>
+              <small> 200+ Worldwide </small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon' />
+              <h5>Projects</h5>
+              <small> 80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum, or lipsum as it is sometimes known, 
+            is dummy text used in laying out print, 
+            graphic or web designs. 
+            The passage is attributed to an unknown typesetter 
+            in the 15th century who is thought to have scrambled 
+            parts of Cicero's De Finibus Bonorum et Malorum for 
+            use in a type specimen book. It usually begins with
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Le`s Talk</a>
+
+        </div>
+      </div> 
+      
+
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './nav.css'
+import { useState } from 'react'
 import {
   AiOutlineHome, 
   AiOutlineUser,
@@ -8,16 +9,22 @@ import {
 } from 'react-icons/ai'
 import {RiServiceLine} from 'react-icons/ri'
 
-const Nav = () => {
-  return (
-    <div>
-      <a href='#home'> <AiOutlineHome/> </a>
-      <a href='#about'> <AiOutlineUser/> </a>
-      <a href='#experience'> <AiOutlineBook/> </a>
-      <a href='#services'> <RiServiceLine/> </a>
-      <a href='#contact'> <AiOutlineMessage/> </a>
 
-    </div>
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#home')
+  return (
+    <nav>
+      <a href='#home' onClick={() => setActiveNav('#home')} 
+      className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome/> </a>
+      <a href='#about' onClick={() => setActiveNav('#about')} 
+      className={activeNav === '#about' ? 'active' : ''}> <AiOutlineUser/> </a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} 
+      className={activeNav === '#experience' ? 'active' : ''}> <AiOutlineBook/> </a>
+      <a href='#services' onClick={() => setActiveNav('#services')} 
+      className={activeNav === '#services' ? 'active' : ''}> <RiServiceLine/> </a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} 
+      className={activeNav === '#contact' ? 'active' : ''}> <AiOutlineMessage/> </a>
+    </nav>
   )
 }
 
