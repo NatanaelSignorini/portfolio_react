320ddcb63e3ea0437ce2b2b55397d73742bf5d8d
Natanael Signorini <natanaelsignorini01@gmail.com>
2022-10-06 14:56:52 -0300

    contact 90%

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './contact.css'
+import emailjs from 'emailjs-com';
 
 import {MdOutlineMail} from 'react-icons/md'
 import {BsWhatsapp} from 'react-icons/bs'
 
+const numberPhone = '5553984363504'
+const whatsappLink = "https://api.whatsapp.com/send?phone="+numberPhone
+const emailLink = "mailto:dummyegator@gmail.com"
+
+const YOUR_SERVICE_ID =''
+const YOUR_TEMPLATE_ID =''
+const YOUR_PUBLIC_KEY =''
+
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm({YOUR_SERVICE_ID}, {YOUR_TEMPLATE_ID}, form.current, {YOUR_PUBLIC_KEY})
+    e.target.reset()
+  };
+
+
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -16,19 +36,20 @@ const Contact = () => {
             <MdOutlineMail className="contact__option-icon"/>
             <h4>Email</h4>
             <h5>dummyegator@gmail.com</h5>
-            <a href="mailto:dummyegator@gmail.com" target="#blank"> Send a message</a>
+            <a href={emailLink} target="#blank"> Send a message</a>
           </article>
 
           <article className="contact__option">
             <BsWhatsapp className="contact__option-icon"/>
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone+5553984363504" target="#blank"> Send a message</a>
+            <a href={whatsappLink} target="#blank"> Send a message</a>
           </article>
 
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" placeholder="Your Message" required />
